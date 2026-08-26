14f32f432b92037c5021bc3dcbf2b9ba0756d1cc
Mounikaambati29 <ambatimounika547@gmail.com>
2026-08-26 15:56:11 +0530

    Add files via upload

---
+++ fundamentals.py
@@ -0,0 +1,35 @@
+#task2
+#Write a program that prints a pattern using multiple print statements
+print("55")
+print("999")
+print("5846")
+print("12345")
+
+#Implement a program that uses a dictionary to store information about a book (title, author, publication year)
+book={"title":"memories", "author":"moni", "publication_year":"2026"}
+print(book)
+print(type(book))
+
+#Write a python program that takes a string as input ( 35 ) and return float value
+student_id="35"
+student_id=float(student_id)
+print(student_id)
+print(type(student_id))
+
+#Write a program to take two names as input and print them together.
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+fullname = firstname +lastname
+print("fullname:",fullname)
+
+#Create a program that takes user input for their age, converts it to an integer, adds 5, and then prints the result
+moni_age = input(" enter your age: ")
+moni_age = int(moni_age) + 5
+print(moni_age)
+print(type(moni_age))
+
+#Build a calculator program that takes two numbers as input and performs the arithmetic operation
+num_1=999
+num_2=854
+result=num_1+num_2
+print("Arithematic operation of two numbers is:",result)
