7b574b95bcd5daa96af40f1940d40239c51b0080
Mounikaambati29 <ambatimounika547@gmail.com>
2026-08-26 15:55:21 +0530

    Add files via upload

---
+++ file_handling.py
@@ -0,0 +1,79 @@
+#mode = 'r'
+# file = open("demo.txt",mode = 'r')
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# file = open("demo.txt",mode = 'r')
+# read_data = file.readline()
+# print(read_data)
+# file.close()
+
+# file = open("demo.txt",mode = 'r')
+# read_data = file.readlines()
+# print(read_data)
+# file.close()
+
+# read --> total content (data)
+# readlines --> total data in list format (list of substrings)
+# readline --> single line (first line)
+
+
+# #mode = 'a'
+# file = open("demo.txt",mode = 'a')
+# write_data = file.write("\nthis is append operation by using mode a")
+# file.close()
+
+# file = open("demo1.txt",mode = 'a')
+# write_data = file.write("\nthis is append operation by using mode a")
+# file.close()
+
+# #mode = 'w'
+# voter_data = ["nikki\n","jithu\n","revi\n","nivi\n","suha\n"]
+# file = open("demo2.txt",mode = 'w')
+# write_data = file.writelines(voter_data)
+# file.close()
+
+#  #mode = 'w+'
+file = open("demo3.txt",mode = 'w+')
+write_data = file.write("pythonlife")
+print(file.tell())
+file.seek(0)
+read_data = file.read()
+print(read_data)
+file.close()
+
+
+# import os
+# fn = "demo.txt"
+# nn = "sample.txt"
+# os.rename(fn,nn)
+
+
+# import os
+# fn = "demo1.txt"
+# nn = "sample1.txt"
+# os.rename(fn,nn)
+
+
+# import os
+# os.remove("sample.txt")
+# os.remove("sample1.txt")
+# os.remove("demo2.txt")
+# os.remove("demo3.txt")
+
+
+# file = open("C:\\Users\\Mounika\\Desktop\\python1234.txt",mode='r)
+# read_data = file.read()
+# print(read_data)
+# file.close()
+
+# import csv
+
+
+# import pypdf
+# import gtts
+# import ffmpeg
+
+# import pypdf2
+# import gtts
