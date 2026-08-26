8358f9d91643af48ba85d42229e1f5ea0ba1c0bf
Mounikaambati29 <ambatimounika547@gmail.com>
2026-08-26 15:40:40 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,63 @@
+#task3
+#python program that prints your name
+print("mounika")
+
+# python script with both single_line and multi_line commments explaining the program 
+num_1=26  #Declare a variable num_1 with value 26
+num_2=52  #Declare a variable num_2 wiht value 52
+result=num_1+num_2  #Declare a variable results and stores addition value
+print("addition value is:",result) 
+
+'''
+Above code prints the additon of two numbers.
+First declare a variable of num_1 by storing a value of integer class 26
+Second declare a variable of num_2 by storing a value of integer class 52
+And declare another variable result and store the value of addition operator of two variables 
+Finally,print the output by using print function.
+'''
+
+#A list containing three different datatypes
+sample_1=[86,95.2,"pythonlife"]
+print(sample_1)
+print(type(sample_1))
+
+#A set containing employee id's
+sample_2={150,926,418,684,783}
+print(sample_2)
+print(type(sample_2))
+
+# concatenate two strings and print the result
+employee_name="mounika"
+employee_id="286"
+result=employee_name+employee_id
+print("addition of the two strings: ",result)
+
+#Create a variable with a name that is a Python keyword. What happen?observation
+#and="python programming language keyword "
+#print(and)
+
+# Declare two variables, one storing an integer and the other a string.Print their values
+student_name="rushmi"
+student_id=9
+print(student_name)
+print(student_id)
+print(type(student_name))
+print(type(student_id))
+
+#Convert a float to an integer and print the result
+item_price=86.92
+item_price=int(item_price)
+print(item_price)
+print(type(item_price))
+
+#Convert an integer to a string and display the output
+regnumber=177745
+regnumber=str(regnumber)
+print(regnumber)
+print(type(regnumber))
+
+#Take the user's age as input and print a message using that input
+user_age= input("enter the user_age")
+print(user_age)
+print(type(user_age))
+print(id(user_age))
